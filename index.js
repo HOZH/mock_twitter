9bbc323ba89921f179add1f3b4ecd488a0ee2eaa
Hangchao Chen <hangchaochen.k@gmail.com>
2019-10-22 22:34:22 -0400

    UI testing

--- index.js
+++ index.js
@@ -13,17 +13,14 @@ const path = require('path')
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 
-
 const app = express()
 
+// Cookie Options
 app.use(cookieSession({
     name: 'session',
     keys: ['key1', 'key2'],
-
-    // Cookie Options
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
-
 //setup res.body
 app.use(express.json())
 //key=value&key=value
@@ -34,19 +31,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+//request logger
 if (app.get('env') === 'development') {
-    //request logger
     app.use(morgan('tiny'))
     print('morgan is enabled');
 }
 
-
+//Database set up
 mongoose
     .connect('mongodb://127.0.0.1/my_db').then(() => { dbDebugger('Connected to MongoDB...') })
     .catch(err => dbDebugger('could not connect to the mongodb... ', err))
 
 const userSchema = new mongoose.Schema({
-
     username: String,
     email: String,
     uuid: String,
@@ -61,10 +57,10 @@ const itemSchema = new mongoose.Schema({
     content: Object,
     timestamp: { type: Number, default: Date.now() / 1000 }
 })
-
 const User = mongoose.model('User', userSchema)
 const Item = mongoose.model('Item', itemSchema)
 
+//mailer set up
 const smtpTransport = nodemailer.createTransport(({
     port: 25,
     secure: false,
@@ -94,14 +90,12 @@ app.use(function (req, res, next) {
 });
 
 
+//routers
 app.get('/', (req, res) => {
-
     print(res.session)
     loginDebugger(req.session, "session")
-
     req.session.count = (req.session.count || 0) + 1
     loginDebugger(req.session.count)
-
     res.render('index');
 })
 
@@ -109,29 +103,18 @@ app.get('/adduser', (req, res) => {
     res.render("addUser");
 })
 app.post('/adduser', (req, res) => {
-
     adduserDebugger(req.body)
-
     const requestError = isValidateAdduserRequest(req.body)
-
     if (requestError.error) {
         adduserDebugger('error on fetching user request(contents)', requestError.error.message)
-        let obj = { status: "error" };
-        return res.render("addUser", obj);
-        //return res.send({ status: "error" })
+        return res.send("addUser", { status: "error" })
     }
-
     isUsernameEmailUnique(req.body.username, req.body.email).then(value => {
-
         const isUnique = value === 0
         adduserDebugger('username and email are both unique =', isUnique)
-
         if (!isUnique) {
-            let obj = { status: "error" };
-            return res.render("addUser", obj);
-            //return res.send({ status: "error" })
+            return res.send("addUser", { status: "error" })
         }
-
         createUserAndSentEmail(req.body.username, req.body.email, req.body.password, false, req, res)
     })
 })
@@ -186,69 +169,50 @@ app.listen(port, () => {
 
 //validation checkers
 function isValidateAdduserRequest(request) {
-
     const schema = {
-
         username: Joi.string().required(),
         email: Joi.string().email().required(),
         password: Joi.string().required()
-
     }
-
     return Joi.validate(request, schema)
 }
 
 async function isUsernameEmailUnique(username, email) {
-
     const user = await User
         .find()
         .or([{ username: username }, { email: email }])
-
     dbDebugger(username, email, "record found:", user)
-
     return user.length
 }
 
 
 //database operations
 async function createUserAndSentEmail(username, email, password, active, req, res) {
-
     const token = uuid.v4()
-
     const user = new User({
-
         username: username,
         email: email,
         password: password,
         uuid: token,
         active: active
-
     })
     const result = await user.save()
-
     dbDebugger(result)
-
-    // return result
     const text = 'key: <' + token + '>'
     let link = "#"
-
     link = "http://" + req.get('host') + "/verify/" + req.body.email + "/" + token;
     mailOptions = {
         to: req.body.email,
         subject: "mock_twitter Please confirm your Email account",
         html: text
     }
-
     smtpTransport.sendMail(mailOptions, function (error, response) {
         if (error) {
             adduserDebugger('error on sending email', error)
-            // return -1
-            return res.send({ status: "error" })
+            return res.send("addUser", { status: "error" })
 
         } else {
-            return res.send({ status: "OK" })
-
-            // return 0
+            return res.send("verify", { status: "OK" })
         }
     });
 }
@@ -265,11 +229,10 @@ async function activateUser(req, res) {
         }, { new: true })
     dbDebugger(user)
     if (user)
-        return res.send({ status: "OK" })
+        return res.render("login", { status: "OK" });
 
     dbDebugger("something went wrong when update active property")
-    return res.send({ status: "error" })
-
+    return res.send("verify", { status: "error" });
 }
 
 async function loginUser(req, res) {
@@ -286,27 +249,27 @@ async function loginUser(req, res) {
             loginDebugger("log in performed")
             req.session.isLogin = true
             req.session.username = req.body.username
-            return res.send({ status: "OK" })
-
+            return res.render("addItem", { status: "OK" })
         }
 
     loginDebugger('fail to log in', user ? "current account has not been enabled" : "username/password does not match record on the server")
     req.session = null
-    return res.send({ status: "error" })
-
+    return res.render("login", { status: "error" });
 }
 
 async function createItem(req, res) {
 
     if (false === (req.session.username || false)) {
-        additemDebugger('need to login first')
-        return res.send({ status: "error", error: 'just stop asking' })
+        additemDebugger('need to login first');
+        alert('need to login first');
+        return res.render("addItem", { status: "error", error: 'just stop asking' })
     }
 
     if (!req.body['content']) {
 
         additemDebugger('empty content')
-        return res.send({ status: "error", error: 'empty content' })
+        alert("empty content");
+        return res.render("addItem", { status: "error", error: 'empty content' })
 
     }
 
@@ -314,7 +277,8 @@ async function createItem(req, res) {
 
     if (childType !== 'retweet' && childType !== 'reply' && childType !== null) {
         additemDebugger('wrong child type')
-        return res.send({ status: "error", error: 'wrong childType' })
+        alert('wrong child type')
+        return res.render("addItem", { status: "error", error: 'wrong childType' })
 
     }
 
@@ -334,7 +298,7 @@ async function createItem(req, res) {
 
     dbDebugger("~~~~~printing result~~~~~~~:", result)
 
-    return res.send({ status: 'OK', id: token })
+    return res.render("search", { status: 'OK', id: token })
 
 }
 
@@ -353,26 +317,26 @@ async function get_item(req, res) {
 }
 
 async function search_item(req, res) {
-    dbDebugger("in function searching items");
-    dbDebugger("req.body = ", req.body)
+    //dbDebugger("in function searching items");
+    //dbDebugger("req.body = ", req.body)
     timestamp = (req.body.timestamp || Date.now() / 1000);
-    dbDebugger("time stamp is: ", timestamp);
+    //dbDebugger("time stamp is: ", timestamp);
     limit = (req.body.limit || 25);
-    dbDebugger("limit is: ", limit);
+    //dbDebugger("limit is: ", limit);
     const items = await Item.find({})
-    dbDebugger("for loop: ");
+    //dbDebugger("for loop: ");
     let result = []
     if (items) {
         for (let i = 0; i < items.length; i++) {
-            dbDebugger("items : ", i, "tsp: ", items[i].timestamp)
+            //dbDebugger("items : ", i, "tsp: ", items[i].timestamp)
             if (items[i].timestamp <= timestamp) {
-                dbDebugger("pushing item ", i);
-                dbDebugger(items[i]);
+                //dbDebugger("pushing item ", i);
+                //dbDebugger(items[i]);
                 result.push(items[i])
             }
         }
         while (result.length > limit) {
-            dbDebugger("too many items, poping 1");
+            //dbDebugger("too many items, poping 1");
             result.pop();
         }
         dbDebugger("returning items with ok")
@@ -381,7 +345,8 @@ async function search_item(req, res) {
     }
 
     dbDebugger("items not found, error")
-    return res.send({
+    alert("items not found")
+    return res.render("search", {
         status: "error",
         error: "items not found"
     })
