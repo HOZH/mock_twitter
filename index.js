cd5f248bd8962606e609e11b75f03be53f2d101a
hong <hozh96@gmail.com>
2019-10-28 05:22:59 +0000

    fixed html link for verification and splited user routes to its own file

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const print = require('debug')('app:print')
-const adduserDebugger = require('debug')('app:adduser')
-const loginDebugger = require('debug')('app:login')
-const verifyDebugger = require('debug')('app:verify')
+// const adduserDebugger = require('debug')('app:adduser')
+// const loginDebugger = require('debug')('app:login')
+// const verifyDebugger = require('debug')('app:verify')
 const getitemDebugger = require('debug')('app:getitem')
 const additemDebugger = require('debug')('app:additem')
 const searchitemDebugger = require('debug')('app:searchitem')
 const morgan = require('morgan')
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 const uuid = require('uuid');
-const Joi = require('joi')
+// const Joi = require('joi')
 const path = require('path')
 const cookieSession = require('cookie-session')
+
+const userRouter = require('./routers/userRouter')
+const itemRouter = require('./routers/itemRouter')
 
 
 
@@ -60,53 +63,26 @@ if (app.get('env') === 'development') {
     print('morgan is enabled');
 }
 
+
+app.use(userRouter)
+
 const db = require('./db')
-const User = db.User
 const Item = db.Item
-
-
-const smtpTransport = nodemailer.createTransport(({
-    port: 25,
-    secure: false,
-    tls: {
-        rejectUnauthorized: false
-    }
-}));
 
 
 
 
 app.get('/', (req, res) => {
 
-    print(res.session)
-    loginDebugger(req.session, "session")
+    print(req.session, "session")
 
     req.session.count = (req.session.count || 0) + 1
-    loginDebugger(req.session.count)
+    print(req.session.count)
 
     res.send('ok')
 })
 
-app.post('/adduser', (req, res) => {
 
-
-    addUser(req, res)
-
-})
-
-
-app.post('/verify', (req, res) => {
-    activateUser(req, res)
-})
-
-app.post('/login', (req, res) => {
-    loginUser(req, res)
-})
-
-app.post('/logout', (req, res) => {
-    req.session = null
-    res.send({ status: "OK" })
-})
 
 app.post('/additem', (req, res) => {
     addItem(req, res)
@@ -123,142 +99,10 @@ app.post('/search', (req, res) => {
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(process.env.PORT)
-    console.log(`listening on port ${port}`)
+    print(process.env.PORT)
+    print(`listening on port ${port}`)
 })
 
-//validation checkers
-function isValidateAdduserRequest(req) {
-
-    const schema = {
-
-        username: Joi.string().required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().required()
-
-    }
-
-    return Joi.validate(req.body, schema)
-}
-
-async function isUsernameEmailUnique(username, email) {
-
-    const user = await User
-        .find()
-        .or([{ username: username }, { email: email }])
-
-    adduserDebugger(username, email, "record found:", user)
-
-    return user.length
-}
-
-
-//database operations
-async function createUserAndSentEmail(username, email, password, active, req, res) {
-
-    const token = uuid.v4()
-
-    const user = new User({
-
-        username: username,
-        email: email,
-        password: password,
-        uuid: token,
-        active: active
-
-    })
-    const result = await user.save()
-
-    adduserDebugger(result)
-
-    // return result
-    const text = 'key: <' + token + '>'
-    let link = "#"
-
-    link = "http://" + req.get('host') + "/verify/" + req.body.email + "/" + token;
-    mailOptions = {
-        to: req.body.email,
-        subject: "mock_twitter Please confirm your Email account",
-        html: text
-    }
-
-    smtpTransport.sendMail(mailOptions, function (error, response) {
-        if (error) {
-            adduserDebugger('error on sending email', error)
-            // return -1
-            return res.send({ status: "error" })
-
-        } else {
-            return res.send({ status: "OK" })
-
-            // return 0
-        }
-    });
-}
-
-function addUser(req, res) {
-    const requestError = isValidateAdduserRequest(req)
-
-    if (requestError.error) {
-        adduserDebugger('error on fetching user request(contents)', requestError.error.message)
-        return res.send({ status: "error" })
-    }
-
-    isUsernameEmailUnique(req.body.username, req.body.email).then(value => {
-
-        const isUnique = value === 0
-        adduserDebugger('username and email are both unique =', isUnique)
-
-        if (!isUnique)
-            return res.send({ status: "error" })
-
-        createUserAndSentEmail(req.body.username, req.body.email, req.body.password, false, req, res)
-
-    })
-}
-
-async function activateUser(req, res) {
-
-    verifyDebugger(req.body)
-    const user = await User.findOneAndUpdate({ uuid: req.body.key, email: req.body.email },
-        {
-            $set: {
-                active: true
-            }
-
-        }, { new: true })
-    verifyDebugger(user)
-    if (user)
-        return res.send({ status: "OK" })
-
-    verifyDebugger("something went wrong when update active property")
-    return res.send({ status: "error" })
-
-}
-
-async function loginUser(req, res) {
-
-    loginDebugger(req.body)
-    const user = await User.findOne({ username: req.body.username, password: req.body.password })
-
-    loginDebugger(user)
-
-    if (user)
-
-        if (user.active) {
-
-            loginDebugger("log in performed")
-            req.session.isLogin = true
-            req.session.username = req.body.username
-            return res.send({ status: "OK" })
-
-        }
-
-    loginDebugger('fail to log in', user ? "current account has not been enabled" : "username/password does not match record on the server")
-    req.session = null
-    return res.send({ status: "error" })
-
-}
 
 async function addItem(req, res) {
 
